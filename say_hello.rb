8e272d13085461be882931a7994e60f2532c97b0
rachelnovember3 <rachelnovember3@gmail.com>
2018-07-10 15:40:42 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,8 @@
-# Build your say_hello method here
+your_name= gets.chomp
+my_name= gets.chomp
+
+def say_hello(your_name,my_name)
+  puts "Hello #{your_name}! I'm #{my_name}"
+end
+
+say_hello(your_name,my_name)
